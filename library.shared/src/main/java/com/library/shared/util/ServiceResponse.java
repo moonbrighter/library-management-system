ebee972fddf594160310dd8279ea3caf7b247145
Moon <rickymarcdev@gmail.com>
2026-04-18 10:13:12 +0800

    feat: add ServiceResponse class for standardized API data transfer

---
+++ library.shared/src/main/java/com/library/shared/util/ServiceResponse.java
@@ -0,0 +1,39 @@
+package com.library.shared.util;
+
+public class ServiceResponse {
+    private boolean success;
+    private String message;
+    private String data; // JSON string of data
+
+    public ServiceResponse(boolean success, String message, String data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
+
+
