69f8671b88d7317991ce2824f1d144eca63cba93
Moon <rickymarcdev@gmail.com>
2026-04-18 10:41:34 +0800

    feat: add JsonUtil class for JSON serialization and deserialization

--- library.shared/src/main/java/com/library/shared/util/JsonUtil.java
+++ library.shared/src/main/java/com/library/shared/util/JsonUtil.java
@@ -1,4 +1,16 @@
 package com.library.shared.util;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class JsonUtil {
+    private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    public String toJson(Object object) {
+        return gson.toJson(object);
+    }
+
+    public <T> Object fromJson(String jsonString, Class<T> clazz) {
+        return gson.fromJson(jsonString, clazz);
+    }
 }
