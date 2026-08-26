1a6ecf981d5cf4d5cf4a51e8ee32958fb831110f
Moon <rickymarcdev@gmail.com>
2026-08-26 23:17:46 +0800

    Add JavaFX skeleton

---
+++ src/main/java/com/library/Main.java
@@ -0,0 +1,25 @@
+package com.library;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("Library Management System - Setup Successful!");
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 640, 480);
+
+        stage.setTitle("Library Management System");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
